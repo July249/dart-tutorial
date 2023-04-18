6c9d3aedabd1706c5c4a0a649e59b22622cec911
jihyunglee <jhlee7125@gmail.com>
2023-04-19 00:30:20 +0900

    Var

---
+++ variables/var.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name1 = 'Bob'; // Implicitly typed
+  // name = 1; // Error: A value of type 'int' can't be assigned to a variable of type 'String'.
+
+  String name2 = 'John'; // Explicitly typed
+
+  // note: usually you don't need to explicitly type variables. Normally Dart recommends to use var.
+}
