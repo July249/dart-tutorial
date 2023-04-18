df9dcf5af49ac2b1c7dd438422a66ea45353810c
jihyunglee <jhlee7125@gmail.com>
2023-04-19 00:34:08 +0900

    final

---
+++ variables/final.dart
@@ -0,0 +1,9 @@
+void main() {
+  // final keyword is used to declare a variable that can be set only once.
+  // Once a final variable is set, it can never be changed.
+  final name = 'Bob';
+  // you can assign type of final variable
+  // final String nickname = 'Bobby'; <- this is not mendatory!
+  // name = 'Alice'; // Error: A final variable can only be set once.
+  print(name);
+}
