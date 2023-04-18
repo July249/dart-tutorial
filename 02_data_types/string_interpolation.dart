2b77e8e36cc72fec96dc08a0ec191557d0abb41b
jihyunglee <jhlee7125@gmail.com>
2023-04-19 01:35:06 +0900

    string interpolation

---
+++ 02_data_types/string_interpolation.dart
@@ -0,0 +1,9 @@
+void main() {
+  var name = 'John';
+  var greeting = 'Hello everyone!, my name is $name, nice to meet you!';
+  print(greeting);
+
+  var age = 10;
+  var myAge = 'I am ${age + 2} years old';
+  print(myAge);
+}
