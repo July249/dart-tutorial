3ffa25cd933f05ba87a781b925477a605e30564f
jihyunglee <jhlee7125@gmail.com>
2023-04-19 17:15:11 +0900

    Sets

--- 02_data_types/sets.dart
+++ 02_data_types/sets.dart
@@ -1,5 +1,8 @@
 void main() {
   // Sets are unordered collections of unique items.
+  // Dart Set -> JS Set Dart Set -> Python Set
+  // Dart List -> JS Array Dart List -> Python List
+
   Set<int> numbers = {1, 2, 3, 4};
   numbers.add(1);
   numbers.add(1);
