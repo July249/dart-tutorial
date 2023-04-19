db332615f3f8e773f4e9b9fdedb783321f6ad691
jihyunglee <jhlee7125@gmail.com>
2023-04-19 17:14:26 +0900

    Sets

---
+++ 02_data_types/sets.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Sets are unordered collections of unique items.
+  Set<int> numbers = {1, 2, 3, 4};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
+}
